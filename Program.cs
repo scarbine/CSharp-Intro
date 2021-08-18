d2a86b6560d2c9636f5494a471a947fcd4eeaa0a
Sam Carbine <scarbine@me.com>
2021-08-18 18:27:16 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+
